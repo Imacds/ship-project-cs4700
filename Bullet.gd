6eff17fa6d25a4524f848902f2080186eec665b8
James T <jamest@earny.co>
2019-03-19 11:28:13 -0700

    chg: ship will spawn in center bottom based on window size

--- Bullet.gd
+++ Bullet.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var MOVE_SPEED = 600
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,4 +11,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	move(delta)
+
+func move(delta):
+	var movement = Vector2()
+	movement.y -= 1
+	move_and_collide(movement * MOVE_SPEED * delta)
--- Ship.gd
+++ Ship.gd
@@ -1,17 +1,22 @@
 extends KinematicBody2D
 
 var MOVE_SPEED = 300
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var VERTICAL_OFFSET = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_init_pos()
+	
+func set_init_pos():
+	var size = OS.get_window_size()
+	var ship_init_pos = Vector2(size.x / 2, size.y - VERTICAL_OFFSET)
+	move_and_slide(ship_init_pos)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	move(delta)
+	
+func move(delta):
 	var move_vec = Vector2()
 	if Input.is_action_pressed("move_down"):
 		move_vec.y += 1
