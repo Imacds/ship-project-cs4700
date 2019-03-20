6f7e8f8a720c1ef190dabf57b5a91e287aebf62f
Cubba Jeron <imacds@gmail.com>
2019-03-20 05:36:05 -0700

    Added in the Enemy Pathing systems

---
+++ Enemy1.gd
@@ -0,0 +1,44 @@
+extends Area2D
+
+# Const and enum variable declarations:
+enum STATE {RAIL, HOLDING, REQUEST_HOLDING}
+
+# Member variable declarations:
+var state = STATE.RAIL
+var oldPos = get_transform().get_origin()
+var teleport = false
+var destroyed = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+
+	if state == STATE.RAIL:
+		if get_parent().unit_offset < 0.95:
+			get_parent().offset += delta * 400
+		else:
+			oldPos = get_global_transform().get_origin()
+			teleport = get_parent().get_parent().teleportOnEnd
+			state = STATE.REQUEST_HOLDING
+			#print("Enemy ready for holding")
+			
+	elif state == STATE.REQUEST_HOLDING:
+		pass
+	elif state == STATE.HOLDING:
+		pass
+		
+func tween(var target):
+	var tween = get_node("Tween")
+	tween.interpolate_property(self, "position", position, target, 3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	tween.start()
+	
+func destroy():
+	if state == STATE.RAIL:
+		get_parent().get_parent().queue_free()
+	elif state == STATE.REQUEST_HOLDING:
+		get_parent().get_parent().queue_free()
+	elif state == STATE.HOLDING:
+		queue_free()
