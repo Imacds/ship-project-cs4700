46d63f465d9f83c1802325bbed2dc18d8e48ddee
James T <github.jtara1@gmail.com>
2019-03-23 22:12:49 -0700

    chg: logic for firing, fire on frame input pressed, count on held input, charged shot fired on frame input released

--- Ship.gd
+++ Ship.gd
@@ -1,16 +1,22 @@
 extends KinematicBody2D
 
+var screen_size
+
+# ship #
 export (int) var speed = 300
 export (int) var vertical_offset = 100 # init offset
-var screen_size
 var ship_size
+var lives = 3
+var invulnerability = 2.0
+# shooting #
+var charge_counter = 0.0 # duration that the fire button has been held
+var fire_cooldown = 0.0 # time since last fired
+export (float) var time_til_charged = 0.75
+export (float) var time_between_shots = 0.25
+# bullet #
 var bullets_container
 var bullets_spawn
 var bullet_resource
-var lives = 3
-var invulnerability = 2.0
-var counter = 0.0
-var cooldown = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -34,18 +40,15 @@ func _physics_process(delta):
 	move(delta)
 	shoot(delta)
 	
-	if cooldown > 0.0:
-		cooldown -= delta
-	
 	if invulnerability > 0.0:
 		invulnerability -= delta
 		get_node("Sprite").modulate = Color(255, 255, 255)
-	elif(counter >= 1.5):
+	elif(charge_counter >= 1.5):
 		get_node("Sprite").modulate = Color(255, 1, 1)
 	else:
 		get_node("Sprite").modulate = Color(1, 1, 1)
 	
-func move(delta):		
+func move(delta):
 	var move_vec = Vector2()
 	if Input.is_action_pressed("move_down"):
 		move_vec.y += 1
@@ -64,23 +67,32 @@ func move(delta):
 	position.y = clamp(position.y, 0 + ship_size.y, screen_size.y - ship_size.y)
 	
 func shoot(delta):
-	if Input.is_action_pressed("shoot"):
-		counter += delta
-	if Input.is_action_just_released("shoot") and cooldown <= 0:
-		var bullet = bullet_resource.instance()
-		bullet.global_position = bullets_spawn.global_position
-		bullet.z_index = 5
-		if(counter >= 1.5):
-			bullet.charged = true
-			bullet.get_node("Sprite").modulate = Color(255, 1, 1)
-			bullet.speed = 800
-			cooldown = 0.0
-		else:
-			cooldown = 0.5
-		counter = 0.0
-		#print(bullet.position)
-		bullets_container.add_child(bullet)
-		counter = 0.0
+	if fire_cooldown > 0.0:
+		fire_cooldown -= delta
+	
+	if Input.is_action_just_pressed("shoot") and fire_cooldown <= 0:
+		create_bullet()
+		fire_cooldown = time_between_shots
+		charge_counter = 0.0
+		
+	elif Input.is_action_pressed("shoot"):
+		charge_counter += delta
+
+	elif Input.is_action_just_released("shoot") and charge_counter >= time_til_charged:
+		create_bullet(true)
+		fire_cooldown = 0.0
+		charge_counter = 0.0
+
+func create_bullet(charged = false):
+	var bullet = bullet_resource.instance()
+	bullet.global_position = bullets_spawn.global_position
+	bullets_container.add_child(bullet)
+	
+	bullet.charged = charged
+	if charged:
+		bullet.get_node("Sprite").modulate = Color(255, 1, 1)
+		bullet.speed = 800
+	return bullet
 
 func _on_Ship_area_entered(area):
 	if "Enemy1" in area.get_name():
