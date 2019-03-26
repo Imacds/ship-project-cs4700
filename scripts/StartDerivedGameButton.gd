21f9a70ad404b886a6daace8f303f4f9f0c5c1c3
Cubba Jeron <imacds@gmail.com>
2019-03-26 02:25:44 -0700

    Fixed bug where Just Ship It wouldn't work after Galaga Clone

--- scripts/StartDerivedGameButton.gd
+++ scripts/StartDerivedGameButton.gd
@@ -2,4 +2,5 @@ extends MenuButton
 
 func _pressed():
 	global.score_required_to_unlock_boss = global.score_required_to_unlock_boss_default
+	global.isNotGalagaClone = true
 	get_tree().change_scene("res://Root.tscn")
--- scripts/StartDerivedGameEndless.gd
+++ scripts/StartDerivedGameEndless.gd
@@ -2,4 +2,5 @@ extends MenuButton
 
 func _pressed():
 	global.score_required_to_unlock_boss = 3.402823e+38
+	global.isNotGalagaClone = true
 	get_tree().change_scene("res://Root.tscn")
