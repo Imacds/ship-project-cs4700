09647a8ed998df6ab4c2dd108cc796938a3435e4
Cubba Jeron <imacds@gmail.com>
2019-03-26 00:19:22 -0700

    A few menu tweaks

--- scripts/ScoreRequiredLabel.gd
+++ scripts/ScoreRequiredLabel.gd
@@ -6,7 +6,7 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	text = "Score Required: " + str(get_score_required())
+	text = "Score Required to \nReach Boss in Adventure Mode: " + str(get_score_required())
 
 func get_score_required():
 	return global.score_required_to_unlock_boss
